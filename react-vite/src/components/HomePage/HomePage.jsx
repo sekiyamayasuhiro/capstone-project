df6f9e895460f4bd986d0d039733886f5868a895
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2024-06-10 23:17:34 -0700

    worked on homepage jsx and css.

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -34,16 +34,48 @@ function HomePage() {
                     />
                 </div>
                 <div className="homepage-top-section-right">
-                    <h1>Welcome to My Fantasy Notebook!</h1>
+                    <h1>Welcome to MyFantasyNotebook!</h1>
                     <p>
-                        Blah blah blah. Blah blah blah. Blah blah blah. Blah
-                        blah blah. Blah blah blah. Blah blah blah. Blah blah
-                        blah. Blah blah blah. Blah blah blah. Blah blah blah.
+                        Explore the ultimate fantasy basketball experience with
+                        MyFantasyNotebook, where up-to-date NBA data,
+                        customizable leagues, and advanced scouting tools come
+                        together to elevate your strategy. Create and manage
+                        your leagues and teams, enjoy detailed stats and player
+                        comparisons, and enrich your fantasy sports journey.
+                        Enhance your engagement by personalizing player profiles
+                        with scouting reports and adding memorable images to
+                        your gallery! Start building your championship team
+                        today and experience fantasy basketball like never
+                        before!
                     </p>
                 </div>
             </section>
+            <section className="homepage-bottom-section">
+                <div>Image Link 1</div>
+                <div>Image Link 2</div>
+                <div>Image Link 3</div>
+            </section>
             <footer className="homepage-footer">
-                <h3>Random footer stuff.</h3>
+                <div>© 2024 MyFantasyNotebook</div>
+                <div>
+                    <a href="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/143.png">
+                        About Me
+                    </a>
+                </div>
+                <div>
+                    <a href="https://github.com/sekiyamayasuhiro">GitHub</a>
+                </div>
+                <div>
+                    <a href="mailto:sekiyamayasuhiro@gmail.com">Contact Me</a>
+                </div>
+            </footer>
+            <footer>
+                <div className="homepage-footer-disclaimer">
+                    This application uses real NBA data and images from NBA.com.
+                    All rights and trademarks belong to their respective owners.
+                    This project is for educational purposes only and is not
+                    affiliated with the NBA.
+                </div>
             </footer>
         </div>
     );
