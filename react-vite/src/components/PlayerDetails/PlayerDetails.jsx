abea9359a599e55aacade7d23825e83eb658837a
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2024-07-15 20:13:41 -0700

    was in wrong directory

--- react-vite/src/components/PlayerDetails/PlayerDetails.jsx
+++ react-vite/src/components/PlayerDetails/PlayerDetails.jsx
@@ -4,9 +4,9 @@ import { useParams } from "react-router-dom";
 const PlayerDetails = () => {
     const { playerId } = useParams();
     const [playerData, setPlayerData] = useState(null);
-    const [careerStats, setCareerStats] = useState(null);
+    const [seasonCurrentStats, setSeasonCurrentStats] = useState(null);
     const [seasonStats, setSeasonStats] = useState(null);
-    const [season2023Stats, setSeason2023Stats] = useState(null);
+    const [careerStats, setCareerStats] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -20,15 +20,15 @@ const PlayerDetails = () => {
             }
         };
 
-        const fetchCareerStats = async () => {
+        const fetchSeasonCurrentStats = async () => {
             try {
                 const response = await fetch(
-                    `/api/players/stats/career/${playerId}`
+                    `/api/players/stats/current/${playerId}`
                 );
                 const data = await response.json();
-                setCareerStats(data);
+                setSeasonCurrentStats(data);
             } catch (err) {
-                setError("Failed to fetch career stats.");
+                setError("Failed to fetch current season stats.");
             }
         };
 
@@ -44,22 +44,22 @@ const PlayerDetails = () => {
             }
         };
 
-        const fetchSeason2023Stats = async () => {
+        const fetchCareerStats = async () => {
             try {
                 const response = await fetch(
-                    `/api/players/stats/2023-24/${playerId}`
+                    `/api/players/stats/career/${playerId}`
                 );
                 const data = await response.json();
-                setSeason2023Stats(data);
+                setCareerStats(data);
             } catch (err) {
-                setError("Failed to fetch 2023-24 season stats.");
+                setError("Failed to fetch career stats.");
             }
         };
 
         fetchPlayerData();
-        fetchCareerStats();
+        fetchSeasonCurrentStats();
         fetchSeasonStats();
-        fetchSeason2023Stats();
+        fetchCareerStats();
     }, [playerId]);
 
     if (error) return <div>{error}</div>;
@@ -67,22 +67,54 @@ const PlayerDetails = () => {
     return (
         <div className="player-details-container">
             <section className="player-details-upper">
-                <h1>Player Details</h1>
+                {/* <h1>Player Details</h1> */}
                 {playerData && (
-                    <h2>
-                        {playerData.first_name} {playerData.last_name}
-                    </h2>
+                    <div>
+                        <h1>{playerData.DISPLAY_FIRST_LAST}</h1>
+                        <p>
+                            Height: {playerData.HEIGHT} {"|"} Weight:{" "}
+                            {playerData.WEIGHT}
+                        </p>
+                        <p>
+                            Jersey #: {playerData.JERSEY} {"|"} Position:{" "}
+                            {playerData.POSITION}
+                        </p>
+                        <p>
+                            Team: {playerData.TEAM_CITY} {playerData.TEAM_NAME}
+                        </p>
+                    </div>
                 )}
             </section>
             <section className="player-stats-section">
-                <h2>2023-24 Season Stats</h2>
-                {season2023Stats && (
+                {seasonCurrentStats && (
                     <div>
-                        <p>PPG: {season2023Stats.PPG}</p>
-                        <p>RPG: {season2023Stats.RPG}</p>
-                        <p>APG: {season2023Stats.APG}</p>
-                        <p>SPG: {season2023Stats.SPG}</p>
-                        <p>BPG: {season2023Stats.BPG}</p>
+                        <h2>
+                            Current Season Stats ({seasonCurrentStats.SEASON_ID}
+                            )
+                        </h2>
+                        <p>GP: {seasonCurrentStats.GP}</p>
+                        <p>MPG: {seasonCurrentStats.MPG}</p>
+                        <p>FGM: {seasonCurrentStats.FGM}</p>
+                        <p>FGA: {seasonCurrentStats.FGA}</p>
+                        <p>FG%: {seasonCurrentStats.FG_PCT}</p>
+                        <p>FG3M: {seasonCurrentStats.FG3M}</p>
+                        <p>FG3A: {seasonCurrentStats.FG3A}</p>
+                        <p>FG3%: {seasonCurrentStats.FG3_PCT}</p>
+                        <p>FTM: {seasonCurrentStats.FTM}</p>
+                        <p>FTA: {seasonCurrentStats.FTA}</p>
+                        <p>FT%: {seasonCurrentStats.FT_PCT}</p>
+                        <p>PTS: {seasonCurrentStats.PTS}</p>
+                        <p>PPG: {seasonCurrentStats.PPG}</p>
+                        <p>REB: {seasonCurrentStats.REB}</p>
+                        <p>RPG: {seasonCurrentStats.RPG}</p>
+                        <p>AST: {seasonCurrentStats.AST}</p>
+                        <p>APG: {seasonCurrentStats.APG}</p>
+                        <p>STL: {seasonCurrentStats.STL}</p>
+                        <p>SPG: {seasonCurrentStats.SPG}</p>
+                        <p>BLK: {seasonCurrentStats.BLK}</p>
+                        <p>BPG: {seasonCurrentStats.BPG}</p>
+                        <p>TOV: {seasonCurrentStats.TOV}</p>
+                        <p>TPG: {seasonCurrentStats.TPG}</p>
                     </div>
                 )}
             </section>
@@ -92,11 +124,29 @@ const PlayerDetails = () => {
                     seasonStats.map((season, index) => (
                         <div key={index}>
                             <h3>{season.SEASON_ID}</h3>
+                            <p>GP: {season.GP}</p>
+                            <p>MPG: {season.MPG}</p>
+                            <p>FGM: {season.FGM}</p>
+                            <p>FGA: {season.FGA}</p>
+                            <p>FG%: {season.FG_PCT}</p>
+                            <p>FG3M: {season.FG3M}</p>
+                            <p>FG3A: {season.FG3A}</p>
+                            <p>FG3%: {season.FG3_PCT}</p>
+                            <p>FTM: {season.FTM}</p>
+                            <p>FTA: {season.FTA}</p>
+                            <p>FT%: {season.FT_PCT}</p>
+                            <p>PTS: {season.PTS}</p>
                             <p>PPG: {season.PPG}</p>
+                            <p>REB: {season.REB}</p>
                             <p>RPG: {season.RPG}</p>
+                            <p>AST: {season.AST}</p>
                             <p>APG: {season.APG}</p>
+                            <p>STL: {season.STL}</p>
                             <p>SPG: {season.SPG}</p>
+                            <p>BLK: {season.BLK}</p>
                             <p>BPG: {season.BPG}</p>
+                            <p>TOV: {season.TOV}</p>
+                            <p>TPG: {season.TPG}</p>
                         </div>
                     ))}
             </section>
@@ -104,11 +154,29 @@ const PlayerDetails = () => {
                 <h2>Career Stats</h2>
                 {careerStats && (
                     <div>
+                        <p>GP: {careerStats.GP}</p>
+                        <p>MPG: {careerStats.MPG}</p>
+                        <p>FGM: {careerStats.FGM}</p>
+                        <p>FGA: {careerStats.FGA}</p>
+                        <p>FG%: {careerStats.FG_PCT.toFixed(3)}</p>
+                        <p>FG3M: {careerStats.FG3M}</p>
+                        <p>FG3A: {careerStats.FG3A}</p>
+                        <p>FG3%: {careerStats.FG3_PCT.toFixed(3)}</p>
+                        <p>FTM: {careerStats.FTM}</p>
+                        <p>FTA: {careerStats.FTA}</p>
+                        <p>FT%: {careerStats.FT_PCT.toFixed(3)}</p>
+                        <p>PTS: {careerStats.PTS}</p>
                         <p>PPG: {careerStats.PPG}</p>
+                        <p>REB: {careerStats.REB}</p>
                         <p>RPG: {careerStats.RPG}</p>
+                        <p>AST: {careerStats.AST}</p>
                         <p>APG: {careerStats.APG}</p>
+                        <p>STL: {careerStats.STL}</p>
                         <p>SPG: {careerStats.SPG}</p>
+                        <p>BLK: {careerStats.BLK}</p>
                         <p>BPG: {careerStats.BPG}</p>
+                        <p>TOV: {careerStats.TOV}</p>
+                        <p>TPG: {careerStats.TPG}</p>
                     </div>
                 )}
             </section>
